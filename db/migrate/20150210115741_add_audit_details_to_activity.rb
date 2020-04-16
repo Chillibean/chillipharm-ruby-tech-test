0becb9d5d2cd38d6b1907c425762d6e25454d37d
Анна Разумова <a.razumova@fun-box.ru>
2020-04-16 18:26:10 +0100

    Fix migrations

--- db/migrate/20150210115741_add_audit_details_to_activity.rb
+++ db/migrate/20150210115741_add_audit_details_to_activity.rb
@@ -1,4 +1,4 @@
-class AddAuditDetailsToActivity < ActiveRecord::Migration
+class AddAuditDetailsToActivity < ActiveRecord::Migration[5.0]
   def change
     add_column :activities, :ip, :string
     add_column :activities, :browser, :string
